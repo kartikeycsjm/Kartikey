ee9b9033c85bb0370efcb3fbdad4e3bf01f23f8e
Kartikey Mishra <kartikeymishracsjm@gmail.com>
2025-05-05 19:30:49 +0530

    added some icons

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -1,3 +1,6 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 export default function Footer() {
   return (
     <footer className="border-t border-zinc-800 
@@ -9,17 +12,20 @@ export default function Footer() {
             {new Date().getFullYear()} Kartikey Mishra. All
             rights reserved.</p>
           <div className="flex gap-6">
-            <a href="https://www.instagram.com/myselfkartikey/" className="text-gray-400 
-              hover:text-white">
-              Instagram
+            <a href="https://www.instagram.com/myselfkartikey/"
+              className="text-gray-400 text-2xl md:text-4xl
+              hover:text-white duration-500">
+              <FaInstagram />
             </a>
-            <a href="#" className="text-gray-400 
-              hover:text-white">
-              Twitter
+            <a href="https://x.com/myself_kartik_"
+              className="text-gray-400 duration-500 
+              hover:text-white text-2xl md:text-4xl">
+              <FaXTwitter />
             </a>
             <a href="https://www.linkedin.com/in/kartikey-mishra-637a442b5/" className="text-gray-400 
-              hover:text-white">
-              LinkedIn
+              hover:text-white text-2xl md:text-4xl
+              duration-500">
+              <FaLinkedin />
             </a>
           </div>
         </div>
